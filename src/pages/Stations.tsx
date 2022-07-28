a2974fc22f1c10f1593468cda3c53736ceed7931
Shamsur <shamsur314@gmail.com>
2022-07-28 18:32:19 +0300

    using axios instead of fetch

--- src/pages/Stations.tsx
+++ src/pages/Stations.tsx
@@ -1,3 +1,4 @@
+import axios from "axios"
 import { useEffect, useState } from "react"
 import { Loading } from "../components/Loading/Loading"
 import StationCard from "../components/Station/StationCard"
@@ -6,19 +7,22 @@ import { IStation } from "../types"
 
 export const Stations = () => {
   const [stations, setStations] = useState<IStation[]>([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
-    fetch("https://bike-app-rest-api.herokuapp.com/api/station")
-      .then(response => response.json())
-      .then(data => {
-        setStations(data)
-      })
+    setLoading(true)
+    axios.get("https://bike-app-rest-api.herokuapp.com/api/station")
+      .then(response => {
+        setStations(response.data.stations)
+        setLoading(false)
+      }
+      )
   }, [])
 
   return (
     <>
-      {!stations.length && <Loading />}
-      {stations.map(station => (
+      {loading && <Loading />}
+      {!loading && stations.map(station => (
         <StationCard key={station.id} station={station} />
       ))}
     </>
