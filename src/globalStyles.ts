b0056ce8aa137ab25ce002a74ba041ccf945e9b1
Shamsur <shamsur314@gmail.com>
2022-07-29 11:36:39 +0300

    font import manually to avoid styled-component warning

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=Edu+VIC+WA+NT+Beginner&display=swap');
-     
     *{
         font-family: 'Edu VIC WA NT Beginner', cursive;
     }
