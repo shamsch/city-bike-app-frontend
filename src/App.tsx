ced19290d261d76faaadf11c0a0e2de69c4bf0db
Shamsur Chowdhury <shamsur.chowdhury@crowdsorsa.com>
2022-08-04 12:08:10 +0300

    added routing from home page to the forms

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,24 @@ import { Journeys } from "./pages/Journeys";
 import { Stations } from "./pages/Stations";
 import { Station } from "./pages/Station";
 import { GlobalStyles } from "./globalStyles";
+import { AddJourney } from "./components/AddForm/AddJourney";
+import { AddStation } from "./components/AddForm/AddStation";
 
 function App() {
-  return (
-    <>
-      <GlobalStyles />
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/stations" element={<Stations />} />
-        <Route path="/stations/:id" element={<Station />} />
-        <Route path="/journey" element={<Journeys />} />
-      </Routes>
-    </>
-
-  );
+	return (
+		<>
+			<GlobalStyles />
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/stations" element={<Stations />} />
+				<Route path="/stations/:id" element={<Station />} />
+				<Route path="/journey" element={<Journeys />} />
+				<Route path="/add-journey" element={<AddJourney />} />
+				<Route path="/add-station" element={<AddStation />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,44 @@
-import { AddJourney } from "../components/AddForm/AddJourney";
-import { AddStation } from "../components/AddForm/AddStation";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { colors } from "../utils/color";
 
 export const Home = () => {
 	return (
-		<div>
-			{/* <AddJourney /> */}
-			<AddStation />
-		</div>
+		<StyledHome>
+			<div>
+				<Link to="/add-journey">Add Journey</Link>
+			</div>
+			<div>
+				<Link to="/add-station">Add Station</Link>
+			</div>
+		</StyledHome>
 	);
 };
+
+const StyledHome = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: space-around;
+	height: 100vh;
+	margin: 10px;
+	padding: 10px;
+
+	div > a {
+		color: black;
+		text-decoration: none;
+		background-color: ${colors.gray};
+		border: none;
+		padding: 15px 32px;
+		text-align: center;
+		text-decoration: none;
+		display: inline-block;
+		font-size: 16px;
+		margin: 4px 2px;
+		cursor: pointer;
+
+		&:hover {
+			background-color: ${colors.grayDark};
+		}
+	}
+`;
