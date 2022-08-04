5c85fd254780a5b031d3fcae789899cab8e437f1
Shamsur Chowdhury <shamsur.chowdhury@crowdsorsa.com>
2022-08-04 13:50:21 +0300

    issue with paginate when filter applied fixed

--- src/pages/Journeys.tsx
+++ src/pages/Journeys.tsx
@@ -10,56 +10,105 @@ import { IResponseJourney } from "../types";
 import { journeyColumns, order } from "../utils/optionsForSelect";
 
 export const Journeys = () => {
-  const [journeys, setJourneys] = useState<IResponseJourney[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [page, setPage] = useState(1);
-  const [totalPage, setTotalPage] = useState(1);
-  const [searchValue, setSearchValue] = useState("");
-  const [distanceRange, setDistanceRange] = useState([0, 5000]); // because I know the max is 4.8k km by SQL query
-  const [durationRange, setDurationRange] = useState([0, 3000]); // because I know the max is 2.7k min by SQL query, however not optimal, i think a backend endpoint for getting the max is needed for more sophisticated approach
-  const [orderBy, setOrderBy] = useState(journeyColumns[0]);
-  const [orderDirection, setOrderDirection] = useState(order[0]);
+	const [journeys, setJourneys] = useState<IResponseJourney[]>([]);
+	const [loading, setLoading] = useState(false);
+	const [page, setPage] = useState(1);
+	const [totalPage, setTotalPage] = useState(1);
+	const [searchValue, setSearchValue] = useState("");
+	const [distanceRange, setDistanceRange] = useState([0, 5000]); // because I know the max is 4.8k km by SQL query
+	const [durationRange, setDurationRange] = useState([0, 3000]); // because I know the max is 2.7k min by SQL query, however not optimal, i think a backend endpoint for getting the max is needed for more sophisticated approach
+	const [orderBy, setOrderBy] = useState(journeyColumns[0]);
+	const [orderDirection, setOrderDirection] = useState(order[0]);
 
-  useEffect(() => {
-    setLoading(true);
-    axios.get("https://bike-app-rest-api.herokuapp.com/api/journey", {
-      params: {
-        page: page,
-        search: searchValue,
-        distanceMin: distanceRange[0],
-        distanceMax: distanceRange[1],
-        durationMin: durationRange[0],
-        durationMax: durationRange[1],
-        orderBy: orderBy.value,
-        orderDir: orderDirection.value
-      },
-    }).then(response => {
-      setJourneys(response.data.journeys);
-      setTotalPage(response.data.total_pages);
-      setLoading(false);
-    })
-  }, [page, searchValue, distanceRange, durationRange, orderBy, orderDirection]);
+	useEffect(() => {
+		setLoading(true);
+		axios
+			.get("https://bike-app-rest-api.herokuapp.com/api/journey", {
+				params: {
+					page: page,
+					search: searchValue,
+					distanceMin: distanceRange[0],
+					distanceMax: distanceRange[1],
+					durationMin: durationRange[0],
+					durationMax: durationRange[1],
+					orderBy: orderBy.value,
+					orderDir: orderDirection.value,
+				},
+			})
+			.then((response) => {
+				setJourneys(response.data.journeys);
+				setTotalPage(response.data.total_pages);
+				if (page > totalPage) {
+					setPage(totalPage);
+				}
+				setLoading(false);
+			});
+	}, [
+		page,
+		searchValue,
+		distanceRange,
+		durationRange,
+		orderBy,
+		orderDirection,
+	]);
 
-  return (
-    <>
-
-      {loading && <Loading />}
-      {!loading && <Searchbar initialValue={searchValue} placeholder="Search by station i.e Lastenlehto, or month i.e May" onChange={(val) => setSearchValue(val)} />}
-      {!loading && <Range title="Distance" unit="km" max={distanceRange[1]} min={distanceRange[0]} step={0.1} onSubmit={(val) => setDistanceRange(val)} />}
-      {!loading && <Range title="Duration" unit="min" max={durationRange[1]} min={durationRange[0]} step={1} onSubmit={(val) => setDurationRange(val)} />}
-      {!loading && <OrderSelect placeholder="Order by" options={journeyColumns} value={orderBy} onChange={(val) => setOrderBy(val)} />}
-      {!loading && <OrderSelect placeholder="Order direction" options={order} value={orderDirection} onChange={(val) => setOrderDirection(val)} />}
-      {!loading && journeys.map(journey => (
-        <JourneyCard key={journey.id} journey={journey} />
-      ))}
-      {!loading &&
-        <Paginate
-          onPageChange={(page) => setPage(page)}
-          totalPage={totalPage}
-          initialPage={page - 1}
-        />
-      }
-    </>
-  )
-}
-
+	return (
+		<>
+			{loading && <Loading />}
+			{!loading && (
+				<Searchbar
+					initialValue={searchValue}
+					placeholder="Search by station i.e Lastenlehto, or month i.e May"
+					onChange={(val) => setSearchValue(val)}
+				/>
+			)}
+			{!loading && (
+				<Range
+					title="Distance"
+					unit="km"
+					max={distanceRange[1]}
+					min={distanceRange[0]}
+					step={0.1}
+					onSubmit={(val) => setDistanceRange(val)}
+				/>
+			)}
+			{!loading && (
+				<Range
+					title="Duration"
+					unit="min"
+					max={durationRange[1]}
+					min={durationRange[0]}
+					step={1}
+					onSubmit={(val) => setDurationRange(val)}
+				/>
+			)}
+			{!loading && (
+				<OrderSelect
+					placeholder="Order by"
+					options={journeyColumns}
+					value={orderBy}
+					onChange={(val) => setOrderBy(val)}
+				/>
+			)}
+			{!loading && (
+				<OrderSelect
+					placeholder="Order direction"
+					options={order}
+					value={orderDirection}
+					onChange={(val) => setOrderDirection(val)}
+				/>
+			)}
+			{!loading &&
+				journeys.map((journey) => (
+					<JourneyCard key={journey.id} journey={journey} />
+				))}
+			{!loading && (
+				<Paginate
+					onPageChange={(page) => setPage(page)}
+					totalPage={totalPage}
+					initialPage={page - 1}
+				/>
+			)}
+		</>
+	);
+};
